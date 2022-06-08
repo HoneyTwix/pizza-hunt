5c2fbc930219269a3f838faad8c1882fa76dd1a2
HoneyTwix <chocolateenchanted@gmail.com>
2022-06-08 14:18:34 -0500

    reply schemas and routes

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,24 @@
-const {Schema,model} = require('mongoose');
+const {Schema,model,Types} = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
+
+const ReplySchema = new Schema({
+    replyBody: {
+        type:String
+    },
+    replyId: {
+        type: Schema.Types.ObjectId,
+        defalut: () => new Types.ObjectId()
+    },
+    writtenBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+})
 const CommentSchema = new Schema({
     writtenBy: {
         type: String
@@ -9,8 +28,21 @@ const CommentSchema = new Schema({
     },
     createdAt:{
         type: Date,
-        default: Date.now
-    }
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    replies: [ReplySchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+CommentSchema.virtual('replyCount').get(function(){
+    return this.replies.length;
 });
 
 const Comment = model('Comment',CommentSchema
